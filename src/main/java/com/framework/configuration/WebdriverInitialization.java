e78f0970c2db719915c5f7828cf4dc9dca24ebd5
anuj88garg <anuj88garg@gmail.com>
2022-06-24 10:28:47 +0530

    Code Updated

--- src/main/java/com/framework/configuration/WebdriverInitialization.java
+++ src/main/java/com/framework/configuration/WebdriverInitialization.java
@@ -3,32 +3,58 @@ package com.framework.configuration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WebdriverInitialization {
+//	
+	protected WebDriver driver;
+//	
+//	static String webdriverPath = "C:\\Users\\anuj88garg\\.m2\\repository\\org\\seleniumhq\\selenium";
+//	
+//	public static WebDriver chromeDriver(){
+//		
+//		//System.setProperty("webdriver.chrome.driver", webdriverPath +"\\selenium-chrome-driver\\4.1.2\\selenium-chrome-driver-4.1.2.exe");
+//		WebDriverManager.chromedriver().setup();
+//		driver = new ChromeDriver();
+//		
+//		return driver;
+//		
+//	}
+//	
+//public static WebDriver firefoxDriver(){
+//		
+//		System.setProperty("webdriver.gecko.driver", webdriverPath +"\\selenium-firefox-driver\\4.1.2\\selenium-firefox-driver-4.1.2.exe");
+//		driver = new FirefoxDriver();
+//		
+//		return driver;
+//		
+//	}
 	
-	static WebDriver driver;
-	
-	static String webdriverPath = "C:\\Users\\anuj88garg\\.m2\\repository\\org\\seleniumhq\\selenium";
-	
-	public static WebDriver chromeDriver(){
-		
-		//System.setProperty("webdriver.chrome.driver", webdriverPath +"\\selenium-chrome-driver\\4.1.2\\selenium-chrome-driver-4.1.2.exe");
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		
-		return driver;
-		
+//@BeforeTest
+//public void launchBrowser(){
+//	if(System.getProperty("app.browser").equalsIgnoreCase("chrome")){
+//		WebDriverManager.chromedriver().setup();
+//		driver = new ChromeDriver();
+//	}
+//	else if(System.getProperty("app.browser").equalsIgnoreCase("firefox")){
+//		WebDriverManager.firefoxdriver().setup();
+//		driver = new FirefoxDriver();
+//	}
+//}
+	public WebdriverInitialization(){
+		if(System.getProperty("app.browser").equalsIgnoreCase("chrome")){
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.get("https://www.rahulshettyacademy.com/");
+		}
+		else if(System.getProperty("app.browser").equalsIgnoreCase("firefox")){
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
 	}
 	
-public static WebDriver firefoxDriver(){
-		
-		System.setProperty("webdriver.gecko.driver", webdriverPath +"\\selenium-firefox-driver\\4.1.2\\selenium-firefox-driver-4.1.2.exe");
-		driver = new FirefoxDriver();
-		
-		return driver;
-		
-	}
+
 
 }
---
+++ src/main/java/com/ui/pages/HomePage.java
@@ -0,0 +1,31 @@
+package com.ui.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.framework.configuration.WebdriverInitialization;
+
+public class HomePage extends WebdriverInitialization{
+	
+	
+	@FindBy(xpath="//a/img[@src='assets/images/rs_logo.png'][1]")
+	public WebElement rsLogo;
+	
+	@FindBy(xpath="//li/a[text()='Practice'][1]")
+	public WebElement practiceTab;
+	
+	 public HomePage(){
+		 super();
+		 PageFactory.initElements(driver, this);
+	 }
+	 
+	 public boolean LogoVisible(){
+		 return rsLogo.isDisplayed();
+	 }
+	 
+	 public void clickPracticePage(){
+		practiceTab.click();
+	 }
+
+}
